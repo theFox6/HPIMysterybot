c9f8c99882fa880e631fe40693f55c07c0b7de8f
Sommer2019 <Blau.Sommer2019@hpi.uni-potsdam.de>
2019-08-08 15:46:11 +0200

    replaced Schrottbot

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ from telegram.ext import Updater, CommandHandler, Filters, ConversationHandler, 
 with open("token.txt") as token_file:
     token = token_file.read().strip()
 
-NEUTRAL, WAITING = range(2)
+users = {}
 
-def machSchrott(bot, update):
-    update.message.reply_text("Schrott")
-    return NEUTRAL
+UNFINISHED, GETNAME = range(2)
 
+def intro(bot, update):
+    update.message.reply_text("Hallo")
+    update.message.reply_text("Ich bin <epischer botname>.")
+    update.message.reply_text("Wie lautet dein Name?")
+    return GETNAME
+
+def set_name(bot, update):
+    users[update.message.chat_id] = {
+        "name" : update.message.text
+    }
+    update.message.reply_text("Freut mich "+update.message.text)
+    return UNFINISHED
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+'''
 def start_timer(bot, update):
     update.message.reply_text("geht ned")
 
@@ -18,19 +33,17 @@ def delete_timer(bot, update):
 def stop_timer(bot, update):
     update.message.reply_text("unimplemented")
 
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
 def sleep(bot, update):
     echo(bot, update)
+'''
 
 conv_handler = ConversationHandler(
-    entry_points = [CommandHandler('start', machSchrott)],
+    entry_points = [CommandHandler('start', intro)],
     states = {
-        NEUTRAL: [CommandHandler('new',start_timer), MessageHandler(Filters.text, echo)],
-        WAITING: [CommandHandler('off', stop_timer), MessageHandler(Filters.text, sleep)]
+        GETNAME: [MessageHandler(Filters.text, set_name)],
+        UNFINISHED: [MessageHandler(Filters.text, echo)]
     },
-    fallbacks = [CommandHandler('done', delete_timer)]
+    fallbacks = [CommandHandler('reset', intro)]
 
 )
 
@@ -41,6 +54,7 @@ def main():
     dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
+    print("bot ready")
     updater.idle()
 
 if __name__ == '__main__':
