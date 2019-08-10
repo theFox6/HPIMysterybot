82983f177322c52277666721720bc1a46a05cc16
Sommer2019 <Blau.Sommer2019@hpi.uni-potsdam.de>
2019-08-10 12:30:07 +0200

    applied various features
    * added hint inline keyboards
    * added restart authorized user file
    * added the ending

--- hints.py
+++ hints.py
@@ -1,39 +1,32 @@
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle
+from telegram.ext import CallbackQueryHandler
 from threading import Timer
 import logging,users
 
-# TODO use menus instead of keyboards
+def give_hint(bot, chat_id, hint_text):
+    users.all[chat_id]['offeredHint'] = hint_text
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Tipp",callback_data="Tipp")]])
+    bot.send_message(chat_id=chat_id, text="Brauchst du einen Tipp?", reply_markup=reply_markup)
 
-def give_hint(bot, chat_id, message):
-    users.all[chat_id]['offeredHint'] = True
-    reply_markup = ReplyKeyboardMarkup([['Tipp'],['Noch nicht.']], one_time_keyboard=True)
-    bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
-
-def run_timer(bot, chat_id, time, message):
+def run_timer(bot, chat_id, time, hint_text):
     logging.debug("creating hint timer")
-    t = Timer(time, give_hint, args=[bot, chat_id, message])
+    t = Timer(time, give_hint, args=[bot, chat_id, hint_text])
     logging.info("registering timer")
     users.all[chat_id]['hintTimer'] = t
     logging.info("starting timer")
     t.start()
 
-def handle_hint(bot, chat_id, answer, hint_text):
-    user = users.all[chat_id]
+def handle_hint(bot, update):
+    user = users.all[update.effective_message.chat_id]
     if not user['offeredHint']:
-        return False
-    if answer == "Tipp":
-        user['offeredHint'] = False
-        # TODO make removal optional
-        reply_markup = ReplyKeyboardRemove()
-        bot.send_message(chat_id=chat_id, text=hint_text, reply_markup=reply_markup)
-        return True
-    elif answer == "Noch nicht.":
-        bot.send_message(chat_id = chat_id, text = 'OK, du kannst ihn mit "Tipp" später noch bekommen.')
-        return True
-    else:
-        return False
+        return
+    bot.answerCallbackQuery(callback_query_id=update.callback_query.id, text=user['offeredHint'])
+    #bot.send_message(chat_id=update.effective_message.chat_id, text=user['offeredHint'])
+    #user['offeredHint'] = False
 
 def cancel(chat_id):
     user = users.all[chat_id]
     user['offeredHint'] = False
     user['hintTimer'].cancel()
+
+callback_handler = CallbackQueryHandler(handle_hint, pattern="Tipp")
